b11c5c5958cb16bcedac1e6ae32ad5ae8d4db94e
Dmitry <Dmitry@DMITRYS-ASUS>
2017-05-08 01:11:03 +0300

    Add NullLogger

--- DesktopToast/ToastManager.cs
+++ DesktopToast/ToastManager.cs
@@ -28,6 +28,8 @@ namespace DesktopToast
 			if (!OsVersion.IsEightOrNewer)
 				return ToastResult.Unavailable;
 
+			logger = logger ?? Logging.NullLogger.Instance;
+
 			if (request.IsShortcutValid)
 				await CheckInstallShortcut(request, logger);
 
@@ -59,6 +61,8 @@ namespace DesktopToast
 				return ToastResult.Invalid;
 			}
 
+			logger = logger ?? Logging.NullLogger.Instance;
+
 			return await ShowAsync(request, logger);
 		}
 
@@ -79,6 +83,8 @@ namespace DesktopToast
 
 			if (!OsVersion.IsEightOrNewer)
 				return ToastResult.Unavailable;
+
+			logger = logger ?? Logging.NullLogger.Instance;
 
 			return await ShowBaseAsync(document, appId, logger);
 		}
@@ -124,7 +130,7 @@ namespace DesktopToast
 				document = AddAudio(document, request);
 			}
 
-			logger?.Log(LogLevel.Debug, document.GetXml());
+			logger.Log(LogLevel.Debug, document.GetXml());
 
 			return document;
 		}
@@ -311,7 +317,7 @@ namespace DesktopToast
 				appId: request.AppId,
 				activatorId: request.ActivatorId))
 			{
-				logger?.Log(LogLevel.Debug, "Creating {0}", shortcutFilePath);
+				logger.Log(LogLevel.Debug, "Creating {0}", shortcutFilePath);
 
 				shortcut.InstallShortcut(
 					shortcutPath: shortcutFilePath,
@@ -326,7 +332,7 @@ namespace DesktopToast
 
 				var delay = (TimeSpan.Zero < request.WaitingDuration) ? request.WaitingDuration : _waitingDuration;
 
-				logger?.Log(LogLevel.Debug, "Waiting {0}", delay);
+				logger.Log(LogLevel.Debug, "Waiting {0}", delay);
 
 				await Task.Delay(delay);
 			}
@@ -384,7 +390,7 @@ namespace DesktopToast
 			// Wait for the result.
 			var result = await tcs.Task;
 
-			logger?.Log(LogLevel.Debug, "Toast result: {0}", result);
+			logger.Log(LogLevel.Debug, "Toast result: {0}", result);
 
 			toast.Activated -= activated;
 			toast.Dismissed -= dismissed;
